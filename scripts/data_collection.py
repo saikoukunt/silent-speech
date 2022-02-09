efae39d89a0ecfc7cb0bbf1c3944995682bd4fa6
Sai Koukuntla <sai.koukunt@gmail.com>
2022-02-09 15:18:39 -0600

    Timer script done

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -1,6 +1,9 @@
 import timer
 
 tim = timer.Timer("vocalized",5,"test.txt")
-tim.start_word('word')
+
+word_list = ['yes', 'no', 'up', 'down', 'left', 'right']
+
+tim.run(word_list)
 
 tim.mainloop()
