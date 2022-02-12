06473351c1e9537b46fc9291ffe990523d3ad177
lwinger04 <47575851+lwinger04@users.noreply.github.com>
2022-02-12 16:26:51 -0600

    Added envelope smoothing function, tweaked some syntax

--- scripts/garbo_SAD.py
+++ scripts/garbo_SAD.py
@@ -7,51 +7,80 @@ Created on Wed Feb  9 15:30:32 2022
 
 from collections import deque
 import numpy as np
+from statistics import mean
 
-class channel(self,  data, int frame_length):
+class channel():
+    def __init__(self, data, frame_length):
+        super().__init__()
     
-    #frame_length is time
-    self.data_rate = 1
-    self.data_size = frame_length * data_rate
-    self.active = False
-    self.max_power = 0
-    self.min_power= 0 
-    self.max_thresh = 1
-    self.min_thresh = 1
+        #frame_length is time
+        self.data_rate = 1
+        self.data_size = frame_length * self.data_rate
+        self.active = False
+        self.max_power = 0
+        self.min_power= 0 
+        self.max_thresh = 1
+        self.min_thresh = 1
     
-    raw_data = data.deque()
-    
-    
-    
-    def smooth(self):
+        self.raw_data = data.deque()
+       
+        
+        
+    #TO DO: MAKE SURE ARRAY OUT OF BOUNDS CHECK IS SUFFICIENT
+    def smooth(self, raw):
+        raw = list(raw)
+        
+        #large window to average over
+        window = int(self.data_rate * 40)
+        
+        #overlap interval
+        skip = int(self.data_rate * 20)
+        
+        ind1 = 0
+        ind2 = window
+        raw = np.array(raw)
+        
+        while ind1 < self.data_size:
+            
+            #remaining data less than window size, avoid array out of bounds
+            if ind2 > self.data_size:
+                ind2 = int(self.data_size)
+                
+            val = mean(raw[ind1:ind2])
+            raw[ind1:ind2] = val
+            ind1 = ind1 + skip
+            ind2 = ind2 + skip
+            
+        raw = raw.tolist()
+        return raw
+        
         
     def calculate(self, smoothed_envelope):
         smoothed = list(smoothed_envelope)
         return np.abs(np.diff(smoothed))
     
     def inactivity_check(self):
-        data_frame = deque(calculate(smooth(raw_data)))
+        data_frame = deque(self.calculate(self.smooth(self.raw_data)))
         frame = data_frame.leftpop()
         
         if self.active:
-            if frame > (max_power * max_thresh):
+            if frame > (self.max_power * self.max_thresh):
                 self.active = True
             else:
                 self.active = False
-            if frame > max_power:
-                max_power = frame
+            if frame > self.max_power:
+                self.max_power = frame
             
             
         else:
-            if frame < (min_power * min_thresh):
+            if frame < (self.min_power * self.min_thresh):
                 self.active = False
             else:
                 self.active = True
-            if frame < min_power:
-                min_power = frame
+            if frame < self.min_power:
+                self.min_power = frame
             
-        
-        
+            
         
         
         
