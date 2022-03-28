7f1a6f8f2c402b9386bf2142c0402c37772976e2
Sai Koukuntla <sai.koukunt@gmail.com>
2022-03-28 14:11:58 -0500

    Added full word list

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -2,7 +2,42 @@ import timer
 
 tim = timer.Timer("vocalized",5,"test.txt")
 
-word_list = ['yes', 'no', 'yes', 'no', 'yes', 'no', 'yes', 'no' ,'yes', 'no']
+# word_list = ['yes', 'no']
+
+word_list = ["yes", "no",
+"zero",
+"one",
+"two",
+"three",
+"four",
+"five",
+"six",
+"seven",
+"eight",
+"nine",
+"ten",
+"up",
+"down",
+"left",
+"right",
+"north",
+"south",
+"east",
+"west",
+"stop",
+"lamp",
+"table",
+"desk",
+"chair",
+"bed",
+"computer",
+"mouse",
+"stove",
+"microwave",
+"sink",
+"toilet"]
+word_list = word_list * 3
+
 
 tim.run(word_list)
 
