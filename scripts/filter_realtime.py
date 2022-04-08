f286b3afd2a424e99c08f2d949c1e37b027ab19d
Lindia Tjuatja <lindia.tjuatja@gmail.com>
2022-04-08 14:04:47 -0500

    real time test

---
+++ scripts/filter_realtime.py
@@ -0,0 +1,51 @@
+import time
+
+from bitalino import BITalino
+
+# The macAddress variable on Windows can be "XX:XX:XX:XX:XX:XX" or "COMX"
+# while on Mac OS can be "/dev/tty.BITalino-XX-XX-DevB" for devices ending with the last 4 digits of the MAC address or "/dev/tty.BITalino-DevB" for the remaining
+macAddress = "20:19:07:00:80:4C"
+
+# This example will collect data for 5 sec.
+running_time = 5
+
+batteryThreshold = 30
+acqChannels = [0, 1, 2, 3, 4, 5]
+samplingRate = 1000
+nSamples = 10
+digitalOutput_on = [1, 1]
+digitalOutput_off = [0, 0]
+
+# Connect to BITalino
+device = BITalino(macAddress)
+
+# Set battery threshold
+device.battery(batteryThreshold)
+
+# Read BITalino version
+print(device.version())
+
+# Start Acquisition
+device.start(samplingRate, acqChannels)
+
+start = time.time()
+end = time.time()
+while (end - start) < running_time:
+    # Read samples
+    print(device.read(nSamples))
+    end = time.time()
+
+# Turn BITalino led and buzzer on
+device.trigger(digitalOutput_on)
+
+# Script sleeps for n seconds
+time.sleep(running_time)
+
+# Turn BITalino led and buzzer off
+device.trigger(digitalOutput_off)
+
+# Stop acquisition
+device.stop()
+
+# Close connection
+device.close()
