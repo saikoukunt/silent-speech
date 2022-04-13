f1274b45e839fee74a70f5bd7b26642f9b07c8ee
Lindia Tjuatja <lindia.tjuatja@gmail.com>
2022-04-13 15:06:33 -0500

    realtime edit

--- scripts/filter_realtime.py
+++ scripts/filter_realtime.py
@@ -1,9 +1,6 @@
 import time
 import numpy as np
 from scipy.signal import filtfilt, butter, iirnotch
-import Packet
-
-from bitalino import BITalino
 
 def transfer_function1(signal):
   return (((np.array(signal) / 2**10) - 0.5) * 3300) / 1009
@@ -12,43 +9,25 @@ def transfer_function2(signal):
   return (((np.array(signal) / 2**6) - 0.5) * 3300) / 1009
 
 #60Hz Notch Filter for Power Line Noise
-b, a = iirnotch(60, 30, 1000)
+b_notch, a_notch = iirnotch(60, 30, 1000)
 # [Band Pass to demonstrate most prominent frequency range]
 low_cutoff = 20
 high_cutoff = 450
 # Fourth Order Butterworth 
 b, a = butter(10, [low_cutoff, high_cutoff], fs=1000, btype='bandpass')
 
-# The macAddress variable on Windows can be "XX:XX:XX:XX:XX:XX" or "COMX"
-# while on Mac OS can be "/dev/tty.BITalino-XX-XX-DevB" for devices ending with the last 4 digits of the MAC address or "/dev/tty.BITalino-DevB" for the remaining
-macAddress = "20:19:07:00:80:4C"
-
-# This example will collect data for 5 sec.
-
-batteryThreshold = 30
-acqChannels = [0, 1, 2, 3, 4, 5]
-samplingRate = 1000
-nSamples = 240
-
-# Connect to BITalino
-device = BITalino(macAddress)
-
-# Set battery threshold
-device.battery(batteryThreshold)
-
-# Read BITalino version
-print(device.version())
-
-# Start Acquisition
-device.start(samplingRate, acqChannels)
+packet_samples = np.load('sample_buffer.npy')
+print(packet_samples)
 
 while True:
-    timestamp = time.time()
-    # Read samples
-    raw_data = device.read(nSamples)
+    start=time.time()
+    timmestamp = packet_samples[0]
+    raw_data = packet_samples[1:]
     raw_data = transfer_function1(raw_data[:,0:4])
     raw_data = transfer_function2(raw_data[:,4:])
-    notched_data = filtfilt(b, a, raw_data)
-    filtered_data = filtfilt(b, a, notched_data)
-    packet = Packet.packet(timestamp, filtered_data)
-    # Function to send packet here
+    notched_data = filtfilt(b_notch, a_notch, raw_data, axis=0)
+    filtered_data = filtfilt(b, a, notched_data, axis=0)
+    
+    print(filtered_data)
+    print(time.time()-start)
+    # Function to send filtered_data here
