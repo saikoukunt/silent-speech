200c7b68dbdceb2d2ec0b3c5577878efda078ca1
Adithya Ashok <43974328+adithyashok@users.noreply.github.com>
2022-04-19 13:28:35 -0500

    Update acquisition_realtime.py

--- scripts/acquisition_realtime.py
+++ scripts/acquisition_realtime.py
@@ -5,28 +5,25 @@ import numpy as np
 
 class EMGStream:
     def __init__(self):
-        self.sample_buffer = np.zeros(shape=(240, 7))
-        self.macAddress = "20:19:07:00:80:4C"
+        self.sample_buffer = np.zeros(shape=(240, 6))
+        self.macAddress = "20:19:07:00:80:4C" 
 		self.batteryThreshold = 30
-		self.acqChannels = [0, 1, 2, 3, 4, 5]
-		self.samplingRate = 1000
-		self.nSamples = 240
-        self.device = BITalino(macAddress)
+		
+        self.device = BITalino(self.macAddress)
         print(self.device.version())        
 
     def get_buffer(self, output):
         
+        bufferSize = 240
+        channels = [0, 1, 2, 3, 4, 5] #[10, 10, 10, 10, 6, 6] bit
+        samplingRate = 1000 #Hz
         self.device.battery(self.batteryThreshold)
-        self.device.start(self.samplingRate, self.acqChannels)
-        row = 0
+        self.device.start(samplingRate, channels)
+        
+        while True:       
 
-        while True:
-            
-
-            sample = (self.device.read(self.nSamples)
+            sample = (self.device.read(bufferSize)
            	sample = np.delete(sample, [0,4], 1)
-            self.sample_buffer[row, :] = np.array(sample)
-            #np.insert(sample_buffer, row, np.array(sample), 0)
-            # print(sample)
+            self.sample_buffer[0, :] = np.array(sample)
             output.put(self.sample_buffer)
             np.save("sample_buffer", sample_buffer)
