5a5b67113ef88220c28972cf58ec83da1d4972f5
Sai Koukuntla <sai.koukunt@gmail.com>
2022-04-22 17:58:24 -0500

    fuck pykaldi

--- scripts/decoder_realtime.py
+++ scripts/decoder_realtime.py
@@ -1,20 +1,13 @@
-from kaldi.asr import GmmLatticeFasterRecognizer, LatticeFasterDecoderOptions
-from kaldi.util.table import SequentialMatrixReader
 import os
 import scipy.io.wavfile as wavf
+import shutil
 
 class Decoder():
     def __init__(self):
-        decoder_opts = LatticeFasterDecoderOptions()
-        decoder_opts.beam = 13.0
-        decoder_opts.lattice_beam = 6.0
-
-        self.asr = GmmLatticeFasterRecognizer.from_files("final.mdl", "HCLG.fst", "words.txt", decoder_opts)
-        self.feats_rspec = ("ark:compute-mfcc-feats --config=models/silent-speech/conf/mfcc.conf "
-               "scp:wav.scp ark:- |")
         self.utt_num = 0
         self.fs = 1000
         self.wav_path = "./wavs/"
+        self.sess = 0
 
     def rescale_data(self, data):
         # convert back to ADC value
@@ -39,19 +32,32 @@ class Decoder():
                 wavf.write(out_f, self.fs, data)
 
                 # create wav.scp
-                wavscp = open('./wav.scp')
+                wavscp = open('data/online/wav.scp','w')
                 path = os.path.join(os.getcwd(), self.wav_path)
                 fname = f'utt_{self.utt_num}.wav'
                 fullpath = path + '/' + fname
                 wavscp.write(f'utt_{self.utt_num}\t{fullpath}\n')
 
-                # decode
-                with SequentialMatrixReader(self.feats_rspec) as feats_reader:
-                    for (fkey, feats) in feats_reader:
-                        out = self.asr.decode(feats)
-                        print(out["text"])
-                        output.put(out["text"])
+                # create utt2spk
+                u2s = open('data/online/utt2spk','w')
+                u2s.write(f'utt_{self.utt_num}\t{self.sess}\n')
+
+                # clear old shit
+                for f in os.listdir('data/online'):
+                    os.remove(os.path.join('data/online',f))
+
+                for f in os.listdir('exp/tri3a/decode'):
+                    os.remove(os.path.join('exp/tri3a/decode',f))
+                os.rmdir('exp/tri3a/decode')
+
+                # run decoding script 
+                os.system('cmd /k ". ./decoder.sh"') 
+
+                # get the output
+                text = open("output.txt",'r')
+                text = text.read()
+                print(text)
+                output.put(text)
                         
                 self.utt_num += 1
 
-    
